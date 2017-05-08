4d309729ae2a4a7c27534fe09a6793ae20d00ecf
Marcel Araujo <ceceldada@gmail.com>
2017-05-08 11:15:21 -0300

    Fixed missing Socket.IO port.
    Fix.

--- common/socket_logic/index.js
+++ common/socket_logic/index.js
@@ -1,5 +1,5 @@
 import io from 'socket.io-client'
-let socket = io('http://localhost')
+let socket = io('http://localhost:8080')
 
 function emitActionToServer(data) {
     socket.emit('event_from_client', 1)
